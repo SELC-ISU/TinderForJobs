d5054a7046d9098f62cf75f6f634f80e5f6a18de
andmarek <andmarek@outlook.com>
2020-03-02 09:10:24 -0600

    random color example

--- example1/ex1.js
+++ example1/ex1.js
@@ -1,4 +1,20 @@
+/* Conventional JavaScript functions.
+ * Could also use arrow functions, but it's not necessary for this
+ * level of simplicity
+ */
 function changeColor() {
+    console.log("Color changed");
     o = document.getElementById("square");
 
+    var x = Math.floor(Math.random() * 256);
+    var y = Math.floor(Math.random() * 256);
+    var z = Math.floor(Math.random() * 256);
+
+
+    bgColor = "rgb(" + x + "," + y + "," + z + ")";
+
+
+    o.style.backgroundColor = bgColor;
 }
+
+
