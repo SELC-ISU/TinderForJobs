fc824ff46ec782edfc246bc34e4066b8667b129e
andmarek <andmarek@outlook.com>
2020-03-02 18:11:54 -0600

    navbar example 2

--- example1/ex1.js
+++ example1/ex1.js
@@ -3,14 +3,20 @@
  * level of simplicity
  */
 function changeColor() {
+    /* You can log things.  Kinda like a printf / System.out.print */
     console.log("Color changed");
-    o = document.getElementById("square");
 
+    /* Getting whatever document is being referenced and then parsing for a specific element
+     * ID or class */
+    o = document.getElementById("square"); // o for object
+
+    /* Just some math */
     var x = Math.floor(Math.random() * 256);
     var y = Math.floor(Math.random() * 256);
     var z = Math.floor(Math.random() * 256);
 
 
+    /* Notice how we don't declare this guy's type */
     bgColor = "rgb(" + x + "," + y + "," + z + ")";
 
 
