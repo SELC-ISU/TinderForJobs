fa3e762da2a4541c3090c99a3efef2c64eeabee2
dunnt <dunntanner06@gmail.com>
2020-03-27 10:57:42 -0500

    boom

--- app/src/main/java/com/example/tfjv2/MainActivity.java
+++ app/src/main/java/com/example/tfjv2/MainActivity.java
@@ -38,6 +38,8 @@ public class MainActivity extends AppCompatActivity {
         al.add("Ryan 'MIS' Herren");
         al.add("Andrew & Alexis <3");
         al.add("Tanner is a fucking dumbass");
+        al.add("Who integrated swipe cards Ryan??? also learn how to spell \"dumass\"");
+
 
         arrayAdapter = new ArrayAdapter<>(this, R.layout.item, R.id.helloText, al );
 
